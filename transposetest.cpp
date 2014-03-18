2985a16941b8a0bd4a92b7491c880c6817fd4d37
Håkon Åmdal <hakon@aamdal.com>
2014-03-18 10:00:40 +0100

    tester for scount og sdispl. sdispl kjører ikke

--- transposetest.cpp
+++ transposetest.cpp
@@ -91,6 +91,68 @@ TEST(create_SIZES, UnEven)
 	ASSERT_EQ(2, s[3]);
 }
 
+TEST(create_Scount, 3x3_2p)
+{
+	int s[2] = {1, 2};
+	int *sC1 = create_Scount(0, 2, s);
+	ASSERT_EQ(1, sC1[0]);
+	ASSERT_EQ(2, sC1[1]);
+	int *sC2 = create_Scount(1, 2, s);
+	ASSERT_EQ(2, sC2[0]);
+	ASSERT_EQ(4, sC2[1]);
+}
+
+TEST(create_Scount, 7x7_3p)
+{
+	int s[3] = {2, 2, 3};
+	int *sC1 = create_Scount(0, 3, s);
+	ASSERT_EQ(4, sC1[0]);
+	ASSERT_EQ(4, sC1[1]);
+	ASSERT_EQ(6, sC1[2]);
+	int *sC2 = create_Scount(1, 3, s);
+	ASSERT_EQ(4, sC2[0]);
+	ASSERT_EQ(4, sC2[1]);
+	ASSERT_EQ(6, sC2[2]);
+	int *sC3 = create_Scount(2, 3, s);
+	ASSERT_EQ(6, sC3[0]);
+	ASSERT_EQ(6, sC3[1]);
+	ASSERT_EQ(9, sC3[2]);
+}
+
+TEST(create_Sdispl, 3x3_2p)
+{
+	int s[2] = {1, 2};
+	int *sD1 = create_Sdispl(0, 2, s);
+	ASSERT_EQ(0, sD1[0]);
+	ASSERT_EQ(1, sD1[1]);
+	int *sD2 = create_Sdispl(1, 2, s);
+	ASSERT_EQ(0, sD2[0]);
+	ASSERT_EQ(2, sD2[1]);
+}
+
+TEST(create_Sdispl, 7x7_3p)
+{
+	int s[3] = {2, 2, 3};
+	
+	/* proc 0 */
+	int *sD0 = create_Sdispl(0, 3, s);
+	ASSERT_EQ(0, sD0[0]);
+	ASSERT_EQ(4, sD0[1]);
+	ASSERT_EQ(8, sD0[2]);
+
+	/* proc 1 */
+	int *sD1 = create_Sdispl(1, 3, s);
+	ASSERT_EQ(0, sD1[0]);
+	ASSERT_EQ(4, sD1[1]);
+	ASSERT_EQ(8, sD1[2]);
+
+	/* proc 2 */
+	int *sD2 = create_Sdispl(2, 3, s);
+	ASSERT_EQ(0, sD2[0]);
+	ASSERT_EQ(6, sD2[1]);
+	ASSERT_EQ(12, sD2[2]);
+}
+
 int
 main(int argc, char** argv)
 {
