2e22916a1c24a101510303c3fb05b9e8411bb842
Håkon Åmdal <hakon@aamdal.com>
2014-03-20 16:51:18 +0100

    refactor convergence

--- convergence.c
+++ convergence.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+
 #ifdef HAVE_MPI
 #include <mpi.h>
 #endif
@@ -13,6 +14,10 @@
 #define N_MIN 3
 #define N_MAX 11	
 
+/*
+ * f function for the poisson problem
+ * 5 * pi^2 * sin(pi * x) * sin(2 * pi * y)
+ */
 Real
 f(Real x, Real y)
 {
@@ -21,6 +26,10 @@ f(Real x, Real y)
 	return 5.0 * pi * pi * sin(pi * x) * sin(2.0 * pi * y);
 }
 
+/*
+ * u (reference) function for the poisson problem
+ * 5 * pi^2 * sin(pi * x) * sin(2 * pi * y)
+ */
 Real
 u(Real x, Real y)
 {
@@ -29,10 +38,13 @@ u(Real x, Real y)
 	return sin(pi * x) * sin(2.0 * pi * y);
 }
 
-
+/*
+ * Main function 
+ */
 int
 main(int argc, char** argv)
 {
+	/* get rank variable */
 	int rank;
 	#ifdef HAVE_MPI
 	MPI_Init(&argc, &argv);
@@ -41,22 +53,26 @@ main(int argc, char** argv)
 	rank = 0;
 	#endif
 
-	int i, j;
-	Real umax;
-	
+	/* u_max variable */
+	Real u_max;
+
+	/* header data for gnuplot output */
 	if (rank == 0) {
 		printf("# Convergence test for Problem Set 6 poisson solver\n");
 		printf("# Problem Size\tAbsolute error\n");
 	}
-
+	
+	/* iterating over different problem sizes */
 	for (i = N_MIN; i <= N_MAX; ++i) {
 		j = pow(2, i);
 		umax = poisson_parallel(j, *f, *u);
 		if (rank == 0) {
+			/* only rank 0 have valid result, and will print to file */
 			printf("%i\t%.16e\n", j, umax);
 		}
 	}
 
+	/* finalize mpi */
 	#ifdef HAVE_MPI
 	MPI_Finalize();
 	#endif
